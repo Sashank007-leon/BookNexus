39a69fdea3045e6b08aa595fc9bb56b43f443d0f
Sashank007-leon <sashank.steam1@gmail.com>
2025-09-18 12:44:21 +0545

    Remove .env and node_modules from repo, update .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import path from "path";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
+import path from "path";
 import { connectDB } from './db/connectDB.js';
-import bookRoutes from './routes/bookRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
+import serverStatus from './routes/serverStatus.js';
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/reviews', reviewRoutes);
+app.use('/', serverStatus);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
